66ae287128edab3d82a319d18f91b77cb68d5e7d
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-10-01 17:48:29 +0530

    latest

--- 02-mongo.sh
+++ 02-mongo.sh
@@ -3,13 +3,13 @@
 source ./common.sh
 check_root
 
-cp /home/ec2-user/roboshop-shell/configurations/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/ec2-user/roboshop-shell/configurations/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOG_FILE
 
-systemctl enable mongod
+systemctl enable mongod &>>$LOG_FILE
 
-systemctl start mongod
+systemctl start mongod &>>$LOG_FILE
 
 # Need to change the localhost application ip to another server 127.0.0.1 t0 0.0.0.0
 # vim /etc/mongod.conf
