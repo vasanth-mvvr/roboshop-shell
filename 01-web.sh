a2b2697dfdb23a6dade9d0483846cd4ff1b698ea
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-10-01 19:43:39 +0530

    latest

--- 01-web.sh
+++ 01-web.sh
@@ -4,28 +4,28 @@ source ./common.sh
 check_root
 
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "installing nginx" 
+# VALIDATE $? "installing nginx" 
 
 systemctl enable nginx &>>$LOG_FILE
-VALIDATE $? "enabling nginx" 
+# VALIDATE $? "enabling nginx" 
 
 systemctl start nginx &>>$LOG_FILE
-VALIDATE $? "Running nginx" 
+# VALIDATE $? "Running nginx" 
 
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>$LOG_FILE
-VALIDATE $? "Downloading application code"
+# VALIDATE $? "Downloading application code"
 
 cd /usr/share/nginx/html &>>$LOG_FILE
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 unzip /tmp/web.zip &>>$LOG_FILE
-VALIDATE $? "Removing the default content and extracting the application code" 
+# VALIDATE $? "Removing the default content and extracting the application code" 
 
 
 cp /home/ec2-user/roboshop-shell/configurations/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-VALIDATE $? "changing the directory" 
+# VALIDATE $? "changing the directory" 
 
 systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "Restarted the service"
+# VALIDATE $? "Restarted the service"
 
 
 
